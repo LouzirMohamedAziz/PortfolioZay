d7095eb2481223b6af0d9cbddef35d7221f3f63f
Mohamed Aziz LOUZIR <medaziz.louzir@gmail.com>
2024-01-22 12:10:52 +0100

    img resolve

--- src/components/home/Home.js
+++ src/components/home/Home.js
@@ -1,18 +1,18 @@
-import React from 'react';
-import Style from './Home.module.scss';
-import me from '../../img/self.png';
+import { Box } from "@mui/material";
 import classNames from 'classnames';
+import React from 'react';
+import me from '../../img/self.png';
+import { info } from "../../info/Info";
 import EmojiBullet from "./EmojiBullet";
+import Style from './Home.module.scss';
 import SocialIcon from "./SocialIcon";
-import {Box} from "@mui/material";
-import {info} from "../../info/Info";
 
 export default function Home() {
 
    return (
       <Box component={'main'} display={'flex'} flexDirection={{xs: 'column', md: 'row'}} alignItems={'center'}
            justifyContent={'center'} minHeight={'calc(100vh - 175px)'}>
-         <Box className={classNames(Style.avatar, Style.shadowed)} alt={'image of developer'} style={{background: info.gradient}} component={'img'} src={me} width={{xs: '35vh', md: '40vh'}}
+         <Box className={classNames(Style.avatar, Style.shadowed)} alt={'image of developer'} style={{background: info.gradient}} component={'img'} src={me} width={{xs: '35vh', md: '57vh'}}
               height={{xs: '35vh', md: '40vh'}}
               borderRadius={'50%'} p={'0.75rem'} mb={{xs: '1rem', sm: 0}} mr={{xs: 0, md: '2rem'}}/>
          <Box>
